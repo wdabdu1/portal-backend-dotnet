554cc8a0bf7fbdcf1a387bd19d80476ff6161816
wdabdu1 <wdabdu@gmail.com>
2026-07-29 14:28:56 +0200

    Add ApplicationUser and related access models

---
+++ Models/Identity/ApplicationUser.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using ShippingPortal.Api.Models.Lookups;
+
+namespace ShippingPortal.Api.Models.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string DisplayName { get; set; } = "";
+    public bool IsActive { get; set; } = true;
+    public ICollection<UserBusinessUnitAccess> BusinessUnitAccess { get; set; }
+        = new List<UserBusinessUnitAccess>();
+}
+
+public enum AccessLevel
+{
+    Read = 0,
+    ReadWrite = 1
+}
+
+public class UserBusinessUnitAccess
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = "";
+    public ApplicationUser? User { get; set; }
+    public int BusinessUnitId { get; set; }
+    public BusinessUnit? BusinessUnit { get; set; }
+    public AccessLevel AccessLevel { get; set; } = AccessLevel.Read;
+}
+
+public static class AppRoles
+{
+    public const string Standard = "Standard";
+    public const string Manager = "Manager";
+    public const string SuperUser = "SuperUser";
+    public static readonly string[] All = { Standard, Manager, SuperUser };
+}
