0dacde01dd60fa27c21cc39bad74c3feec94f8d1
wdabdu1 <wdabdu@gmail.com>
2026-07-30 09:05:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -51,7 +51,11 @@ builder.Services
 
 builder.Services.AddScoped<TokenService>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 
 var allowedOrigins = (builder.Configuration["ALLOWED_ORIGINS"] ?? "http://localhost:4200")
