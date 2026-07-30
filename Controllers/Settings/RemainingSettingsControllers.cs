b51a5580b381f57bf1c9b99f2d2ab82ed42d867c
wdabdu1 <wdabdu@gmail.com>
2026-07-30 11:00:38 +0200

    Add multiple settings controllers for API

---
+++ Controllers/Settings/RemainingSettingsControllers.cs
@@ -0,0 +1,58 @@
+using ShippingPortal.Api.Data;
+using ShippingPortal.Api.Models.Lookups;
+
+namespace ShippingPortal.Api.Controllers.Settings;
+
+[Route("api/settings/couriers")]
+public class CouriersController : LookupCrudController<Courier>
+{
+    public CouriersController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/forwarders")]
+public class ForwardersController : LookupCrudController<Forwarder>
+{
+    public ForwardersController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/tenors")]
+public class TenorsController : LookupCrudController<Tenor>
+{
+    public TenorsController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/sender-banks")]
+public class SenderBanksController : LookupCrudController<SenderBank>
+{
+    public SenderBanksController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/receiver-banks")]
+public class ReceiverBanksController : LookupCrudController<ReceiverBank>
+{
+    public ReceiverBanksController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/shipment-destinations")]
+public class ShipmentDestinationsController : LookupCrudController<ShipmentDestination>
+{
+    public ShipmentDestinationsController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/public-holidays")]
+public class PublicHolidaysController : LookupCrudController<PublicHoliday>
+{
+    public PublicHolidaysController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/spc-rates")]
+public class SpcRatesController : LookupCrudController<SpcRate>
+{
+    public SpcRatesController(ShippingPortalDbContext db) : base(db) { }
+}
+
+[Route("api/settings/acd-cost-settings")]
+public class AcdCostSettingsController : LookupCrudController<AcdCostSetting>
+{
+    public AcdCostSettingsController(ShippingPortalDbContext db) : base(db) { }
+}
