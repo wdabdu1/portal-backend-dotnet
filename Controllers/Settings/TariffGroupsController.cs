5e61f9761a735048919dca241720661177b2636c
wdabdu1 <wdabdu@gmail.com>
2026-08-02 09:39:49 +0200

    Add TariffGroupsController for tariff group management

---
+++ Controllers/Settings/TariffGroupsController.cs
@@ -0,0 +1,10 @@
+using ShippingPortal.Api.Data;
+using ShippingPortal.Api.Models.Lookups;
+
+namespace ShippingPortal.Api.Controllers.Settings;
+
+[Route("api/settings/tariff-groups")]
+public class TariffGroupsController : LookupCrudController<TariffGroup>
+{
+    public TariffGroupsController(ShippingPortalDbContext db) : base(db) { }
+}
