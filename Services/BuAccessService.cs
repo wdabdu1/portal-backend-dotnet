4f38968b95f059759c1a1f8c6abbde2485344d5c
wdabdu1 <wdabdu@gmail.com>
2026-08-02 18:01:56 +0200

    Create BuAccessService.cs

---
+++ Services/BuAccessService.cs
@@ -0,0 +1,33 @@
+using System.Security.Claims;
+
+namespace ShippingPortal.Api.Services;
+
+// Central rule for "which Business Unit IDs can this user see":
+// Manager / SuperUser / Clearance bypass entirely (see all BUs).
+// Everyone else (Standard) is limited to their assigned BUs, read from
+// the "bu" claims baked into their JWT at login (format "buId:AccessLevel").
+public class BuAccessService
+{
+    public bool SeesAllBus(ClaimsPrincipal user)
+    {
+        return user.IsInRole(Models.Identity.AppRoles.Manager)
+            || user.IsInRole(Models.Identity.AppRoles.SuperUser)
+            || user.IsInRole(Models.Identity.AppRoles.Clearance);
+    }
+
+    public HashSet<int> GetAllowedBusinessUnitIds(ClaimsPrincipal user)
+    {
+        return user.Claims
+            .Where(c => c.Type == "bu")
+            .Select(c => c.Value.Split(':')[0])
+            .Where(id => int.TryParse(id, out _))
+            .Select(int.Parse)
+            .ToHashSet();
+    }
+
+    public bool CanSeeBusinessUnit(ClaimsPrincipal user, int businessUnitId)
+    {
+        if (SeesAllBus(user)) return true;
+        return GetAllowedBusinessUnitIds(user).Contains(businessUnitId);
+    }
+}
