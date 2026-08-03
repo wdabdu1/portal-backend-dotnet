5d860e1a241fb0a330e59bd708cd204c36a55c88
wdabdu1 <wdabdu@gmail.com>
2026-08-03 13:53:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -65,6 +65,7 @@ builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     });
 builder.Services.AddEndpointsApiExplorer();
 
