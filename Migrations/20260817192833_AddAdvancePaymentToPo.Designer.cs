e6f14c3e5b602876b4ad5efcda0575c733185d07
Mohammed Abdu Fadeil <Wdabdu@MacBook-Air-2.local>
2026-08-17 21:29:48 +0200

    Add PO Advance Payment fields

--- Migrations/20260817192833_AddAdvancePaymentToPo.Designer.cs
+++ Migrations/20260817192833_AddAdvancePaymentToPo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShippingPortal.Api.Data;
 
@@ -11,9 +12,11 @@ using ShippingPortal.Api.Data;
 namespace portal_backend_dotnet.Migrations
 {
     [DbContext(typeof(ShippingPortalDbContext))]
-    partial class ShippingPortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260817192833_AddAdvancePaymentToPo")]
+    partial class AddAdvancePaymentToPo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260817192833_AddAdvancePaymentToPo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace portal_backend_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdvancePaymentToPo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFromPoAdvance",
+                table: "ShipmentPaymentDues",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "AdvancePaymentExecutedDate",
+                table: "PurchaseOrders",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "AdvancePaymentPercent",
+                table: "PurchaseOrders",
+                type: "decimal(65,30)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "AdvancePaymentPlannedDate",
+                table: "PurchaseOrders",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFromPoAdvance",
+                table: "ShipmentPaymentDues");
+
+            migrationBuilder.DropColumn(
+                name: "AdvancePaymentExecutedDate",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "AdvancePaymentPercent",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "AdvancePaymentPlannedDate",
+                table: "PurchaseOrders");
+        }
+    }
+}
