89b8da1f9f6fc5a503f1c57b6c4e254c08d7a711
wdabdu1 <wdabdu@gmail.com>
2026-08-19 09:28:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -101,6 +101,7 @@ builder.Services.AddScoped<BuAccessService>();
 builder.Services.AddScoped<PoAdvancePaymentService>();
 builder.Services.AddScoped<SettingsUploadService>();
 builder.Services.AddScoped<SettingsExportService>();
+builder.Services.AddScoped<CompleteDeleteService>();
 builder.Services.AddScoped<SectionLockService>();
 
 builder.Services.AddControllers()
